c723171bf9e3af32cf1c1b180ed423d8099c324c
saaimmalik <saaimmalik832@gmail.com>
2025-09-29 23:39:27 +0100

    Deploy static portfolio

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,13 +22,13 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "Saaim Malik",
+                1000,
+                "Computer Science Student",
+                1000,
+                "App Developer",
                 1000,
                 "Web Developer",
-                1000,
-                "Mobile Developer",
-                1000,
-                "UI/UX Designer",
                 1000,
               ]}
               wrapper="span"
@@ -37,8 +37,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+            I am a first year university student at University College Dublin trying to make cool things
           </p>
           <div>
             <Link
@@ -63,15 +62,6 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/images/hero-image.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
-            />
-          </div>
         </motion.div>
       </div>
     </section>
